b9ac41a4e99b6e436a04727bec7dab5ea96653f6
rkaysen63 <rkaysen@plant-process.com>
2021-05-22 19:06:05 -0500

    made charts responsive. added marker size, color

--- chart.js
+++ chart.js
@@ -102,7 +102,7 @@ function buildCharts(sample) {
   
     // D1-7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
+    //  so the otu_ids with the most bacteria are last. Can also .map(x => `OTU ${x} `).
     var first10otuIds = otuIds.slice(0, 10).map(x => "OTU " + x + " ").reverse();
     var first10sampleValues = sampleValues.slice(0, 10).reverse();
     var first10otuLabels = otuLabels.slice(0, 10).reverse();
@@ -125,7 +125,7 @@ function buildCharts(sample) {
       title: "Top 10 Bacteria Cultures Found"
     };
 
-    // D4.  Make chart responsive
+    // D4.  Make bar chart responsive
     var barconfig = {responsive: true};
 
     // D1-10. Use Plotly to plot the data with the layout. 
@@ -136,23 +136,20 @@ function buildCharts(sample) {
     var bubbleData = [{
       x: otuIds,
       y: sampleValues,
-      type: "scatter",
+      // type: "bubble",
       mode: "markers",
       hovertext: otuLabels,
       marker: {
-        // size: [sampleValues]}
-        // sizemode="area"
-        // sizeref=2.0*max(sampleValues)/(40.0**2),
-        // sizemin=4
-        color: [otuIds], 
-        colorscale: [(0,"blue"),(1,"red")]}  
-      }];
+        size: sampleValues,
+        color: otuIds,
+        colorscale: "Earth"}  
+    }];
 
     // D2-2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
       xaxis: {title: "OTU ID"},
-      showlegend: true,
+      showlegend: false,
       margin: {
         l: 50,
         r: 50,
@@ -162,16 +159,18 @@ function buildCharts(sample) {
       hovermode: "closest"
     };
 
+    // D4 Make chart responsive
+    var bubbleConfig = {responsive: true};
+
     // D2-3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout, bubbleConfig); 
 
     // D3-4. Create the trace for the gauge chart.
     var gaugeData = [{
       type: "indicator",
       mode: "gauge+number",
       value: washingFreq,
-      title: {text: "Belly Button Washing Frequency",
-      font:{size:36}|"Scrubs per Week", font:{size: 16}},
+      title: {text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week"},
       gauge:{
         axis: {range: [null, 10], tickwidth: 2, tickcolor: "black"},
         bar: {color: "lightcyan"},
